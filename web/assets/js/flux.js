80ae6ea1980231c1284cd42ebd13fbe9e663cb55
quadroloop <nplixel@gmail.com>
2018-11-02 14:26:10 +0800

    Version 0.0.1 finished:     > Added state saving per added account,     > Added toast notification + sound notification.     > Added, copying and deleting address.     > Completed saving list of address being watched,
        project duration for version 0.0.1: 24.8 hrs,
    
        TODO:
        > Log of events
        > Sort function for logs
        > offline NodeJS or Python version of the project

--- web/assets/js/flux.js
+++ web/assets/js/flux.js
@@ -3,12 +3,25 @@
   Author: Bryce C. Mercines 2018
 */
 
+window.onload = () => {
+  if(localStorage.accounts){
+     var accx = JSON.parse(localStorage.accounts);
+     var nlx = JSON.parse(localStorage.namelist);
+   namelist = nlx;
+   accounts = accx;
+
+   updateAccounts();
+ }
+}
+
 var cpage = "dashboard";
 var cwindow = "p_dashboard";
 
 var cAcc;
 
-
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
 
 function updateBanner(text) {
    document.getElementById("banner").innerText = text;
@@ -98,6 +111,9 @@ function addAccount() {
        if(web3.utils.isAddress(address)){
        accounts[name] = address; 
        namelist.push(name);
+       localStorage.cAcc = name;
+       localStorage.namelist = JSON.stringify(namelist);
+       localStorage.accounts = JSON.stringify(accounts);
        swal({
           type: 'success',
           title: 'success',
@@ -117,6 +133,9 @@ function addAccount() {
 
 function selectAccount(id) {
     cAcc = id;
+    cval = 0;
+    gval = 0;
+    lval = 0;
     var name_large = document.getElementById("n_lg");
     var name_small = document.getElementById("n_sm");
     var address_small = document.getElementById("addr_sm");
@@ -124,8 +143,10 @@ function selectAccount(id) {
 
     name_small.innerHTML = id;
     name_large.innerHTML = id;
+    if( accounts[id]){
     address_small.innerHTML = accounts[id][0]+accounts[id][1]+accounts[id][2]+accounts[id][3]+accounts[id][4]+accounts[id][5]+"......."+accounts[id][37]+accounts[id][38]+accounts[id][39]+accounts[id][40]+accounts[id][41];
     address_large.innerHTML = accounts[id];
+   }
 
     document.getElementById("dashboard").click();
 } 
@@ -152,6 +173,8 @@ function clearData(){
     }
    })
 }
+
+
 
 function createAcc() {
    document.getElementById("accounts").click();
@@ -188,10 +211,12 @@ function createAcc() {
           if(cb > cval){
              gval = cb-cval;
              cval = cb;
+             notif("Event: Gained Ether!","success");
           }
           if(cb < cval){
              lval = cval-cb; 
              cval = cb;
+             notif("Event: Lost Ether!","error");
           }
         }else{
           cval = cb;
@@ -207,3 +232,75 @@ function createAcc() {
     }); 
 
   }
+
+var toast_type = {
+  "success":"linear-gradient(to right, #00b09b, #96c93d)",
+  "error": "linear-gradient(to right, #dd1818, #ff8c00)",
+  "default": "linear-gradient(to right, #1EF6BE, #64BBF6);",
+}  
+
+function notif(message,type){
+  Toastify({
+  text: message,
+  duration: 3000,
+  newWindow: true,
+  close: true,
+  gravity: "top",
+  positionRight: true,
+  backgroundColor: toast_type[type],
+  }).showToast();
+
+  var s = document.getElementById("notif");
+    s.play(); 
+
+
+}
+
+function copyAddress(elementId,btn) {
+  var aux = document.createElement("input");
+  aux.setAttribute("value", document.getElementById(elementId).innerHTML);
+  document.body.appendChild(aux);
+  aux.select();
+  document.execCommand("copy");
+  document.body.removeChild(aux);
+
+ btn.innerHTML = `<i class="fa fa-copy mr-1"></i> Copied!`;
+ setTimeout(()=>{
+    btn.innerHTML = `<i class="fa fa-copy mr-1"></i> Copy Address`;
+ },1200);
+}
+
+function deleteAddress() {
+   swal({
+    type: 'info',
+    title: 'Are you sure?',
+    text: 'Deleting an address cannot be undone',
+    showCancelButton: true,
+   }).then((res)=>{
+     if(res.value){
+      swal({
+        type: 'success',
+        title: 'Success',
+        text: 'Address deleted',
+        showConfirmButton: false,
+        timer: 1500
+      });
+      delete accounts[cAcc];
+      var index = namelist.indexOf(cAcc);
+      if (index > -1) {
+       namelist.splice(index, 1);
+      }
+      cAcc = "";    
+      updateAccounts();
+      localStorage.cAcc = name;
+      localStorage.namelist = JSON.stringify(namelist);
+      localStorage.accounts = JSON.stringify(accounts); 
+      var cbalance = document.getElementById("cbalance");
+      var leth = document.getElementById("leth");
+      var geth = document.getElementById("geth");
+         geth.innerHTML = 0;
+         cbalance.innerHTML = 0;
+         leth.innerHTML = 0;
+    }
+   })
+}
