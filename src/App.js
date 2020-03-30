9736af081dca803c6447d83f43f0ab8ae53f96d6
Bryce Mercines <nplixel@gmail.com>
2020-03-30 10:37:18 +0800

    saving UI changes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from 'react'
 import Table from './components/Table'
 import Tabs from './components/Tabs'
 import TraceChart from './components/TraceChart';
+import InfoCard from './components/InfoCard';
 
 const App = () => {
   return (
     <div className="App">
       <div className="top-half">
-        <div className="row">
+        <div className="row d-flex justify-content-center">
           <div className="col-md-8">
             <TraceChart />
+          </div>
+          <div className="col-md-3">
+            <InfoCard />
           </div>
         </div>
       </div>
 
-      <div className="row">
+      <div className="row d-flex justify-content-center">
         <div className="col-md-8 p-3">
           <Table />
         </div>
---
+++ src/components/InfoCard.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const InfoCard = () => {
+  return (
+    <div className="info-card">
+
+      <div className="account-card">
+        <div class="media align-items-center">
+          <a href="/delta" class="avatar avatar-sm rounded-circle mr-3">
+            <img alt="user-x" src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/12/Gradient_builder_2.jpg?auto=format&q=60&w=1815&h=1815&fit=crop&crop=faces" />
+          </a>
+          <div class="media-body">
+            <span class="name mb-0 text-sm">Bryce Mercines</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="account-card">
+        1 ETH + 0.999 ETH
+      </div>
+
+      <div className="account-card">
+        1 ETH + 0.999 ETH
+      </div>
+
+      <div className="account-card">
+        1 ETH + 0.999 ETH
+      </div>
+
+      <div className="account-card">
+        1 ETH + 0.999 ETH
+      </div>
+
+      <div className="account-card">
+        1 ETH + 0.999 ETH
+      </div>
+
+      <div className="account-card">
+        1 ETH + 0.999 ETH
+      </div>
+
+      <div className="account-card">
+        1 ETH + 0.999 ETH
+      </div>
+
+      <div className="account-card">
+        1 ETH + 0.999 ETH
+      </div>
+
+    </div>
+  )
+}
+
+export default InfoCard;
